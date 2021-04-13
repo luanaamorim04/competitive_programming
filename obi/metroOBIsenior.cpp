882103828911c4e8adfe669fe4b9a99f68d95fda
luanaamorim04 <62717843+luanaamorim04@users.noreply.github.com>
2021-04-13 16:19:46 -0300

    bfs

---
+++ obi/metroOBIsenior.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <cstring>
+#include <set>
+#include <bitset>
+#define ll long long
+#define INF (1e9)
+#define MAX (2e5)
+#define MOD 1000000007
+#define par pair<int, int>
+#define all(v) v.begin(), v.end()
+#define lsb(x) (x & -x)
+#define dbug(x) cout << #x << ": " << x << endl
+#define dbugii(x) cout << x.first << ' ' << x.second << endl
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+int n, m, grau[1<<20], resp, a, b;
+vector<int> grafo[1<<20];
+queue<int> fila;
+
+int main()
+{_
+	cin >> n >> m;
+	while (m--)
+	{
+		cin >> a >> b;
+		grau[a]++;
+		grau[b]++;
+		grafo[a].push_back(b);
+		grafo[b].push_back(a);
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (grau[i] > 4 || grau[i] == 1) fila.push(i);
+	}
+
+	while (!fila.empty())
+	{
+		int u = fila.front(); fila.pop();
+		grau[u] = 0;
+		resp++;
+		for (int v : grafo[u])
+			if (grau[v] == 2) fila.push(v);
+	}
+
+	cout << n - resp << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
