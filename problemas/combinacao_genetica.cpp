a92c75cb30c4d78bbfdfe8bc460050dda86d6a13
luanaamorim04 <62717843+luanaamorim04@users.noreply.github.com>
2021-06-28 14:27:09 -0300

    Lucas' theorem

---
+++ problemas/combinacao_genetica.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <cstring>
+#include <set>
+#include <bitset>
+#define ll long long
+#define INF (1e9)
+#define MAX (int) (2e6 + 5)
+#define MOD 1000000007
+#define par pair<int, int>
+#define all(v) v.begin(), v.end()
+#define esq(x) (x<<1)
+#define dir(x) ((x<<1)+1)
+#define lsb(x) (x & -x)
+#define dbug(x) cout << #x << ": " << x << endl
+#define dbugii(x) cout << x.first << ' ' << x.second << endl
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+ll n, resp, arr[MAX], ok;
+
+int main()
+{_
+	cin >> n;
+	for (int i = 0; i < n; i++) 
+	{
+		cin >> arr[i];
+		if (((n-1)&i) == i) resp ^= arr[i], ok |= (arr[i] == -1);
+	}
+
+	cout << (ok ? -1 : resp) << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
