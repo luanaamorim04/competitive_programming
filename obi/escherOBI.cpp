be51b973efa6472605cd9b7df2f071226655fe7b
luanaamorim04 <62717843+luanaamorim04@users.noreply.github.com>
2021-09-03 12:33:56 -0300

    sorting

---
+++ obi/escherOBI.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <cstring>
+#include <set>
+#include <stack>
+#include <bitset>
+#define ll long long
+#define INF (1e9)
+#define MAX (int) (2e5 + 5)
+#define MOD 1000000007
+#define par pair<int, int>
+#define all(v) v.begin(), v.end()
+#define sz(x) (int) ((x).size())
+#define esq(x) (x<<1)
+#define dir(x) ((x<<1)|1)
+#define lsb(x) (x & -x)
+#define W(x) cout << #x << ": " << x << endl
+#define Wii(x) cout << x.first << ' ' << x.second << endl
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+int n, arr[MAX], resp;
+
+int main()
+{_
+	cin >> n;
+	resp = 1;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	for (int i = 0; i <= (n>>1); i++)
+		resp &= (arr[i]+arr[n-1-i] == arr[0]+arr[n-1]);
+
+	cout << (resp?"S":"N") << endl;
+}
+
+
+
+
+
+
+
+
+
+
