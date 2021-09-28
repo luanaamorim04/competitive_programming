d957677cb27b311def72d07ce530e5c65dbe20fa
luanaamorim04 <62717843+luanaamorim04@users.noreply.github.com>
2021-09-27 21:11:14 -0300

    matrix fexpo

---
+++ problemas/plant118.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <cstring>
+#include <set>
+#include <stack>
+#include <bitset>
+#define ll long long
+#define INF (1e9)
+#define MAX 2
+#define MOD 1000000007
+#define par pair<int, int>
+#define all(v) v.begin(), v.end()
+#define sz(x) (int) ((x).size())
+#define esq(x) (x<<1)
+#define dir(x) ((x<<1)|1)
+#define lsb(x) (x & -x)
+#define W(x) cout << #x << ": " << x << endl
+#define Wii(x) cout << x.first << ' ' << x.second << endl
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+ll n;
+
+struct matrix{
+	ll m[2][2];
+};
+
+matrix t;
+
+matrix mult(matrix a, matrix b)
+{
+	matrix r;
+	for (int i = 0; i < MAX; i++)
+		for (int j = 0; j < MAX; j++)
+		{
+			r.m[i][j] = 0;
+			for (int k = 0; k < MAX; k++)
+				r.m[i][j] += ((a.m[i][k]*b.m[k][j]) % MOD);
+			r.m[i][j]%=MOD;
+		}
+	return r;
+}
+
+matrix fexpo(matrix b, ll e)
+{
+	matrix r;
+	for (int i = 0; i < MAX; i++)
+		for (int j = 0; j < MAX; j++)
+			r.m[i][j] = (i==j);
+
+	while (e)
+	{
+		if (e&1) r = mult(r, b);
+		b = mult(b, b);
+		e>>=1;
+	}
+	return r;
+}
+
+int main()
+{_
+	cin >> n;
+	matrix t;
+	t = {{{3, 1}, {1, 3}}};
+	cout << fexpo(t, n).m[0][0] << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
