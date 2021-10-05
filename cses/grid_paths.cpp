7df4ed14d0f08b928e142aa1176587c7df481921
luanaamorim04 <62717843+luanaamorim04@users.noreply.github.com>
2021-10-05 18:46:42 -0300

    dp

--- cses/grid_paths.cpp
+++ cses/grid_paths.cpp
@@ -5,72 +5,80 @@
 #include <vector>
 #include <cmath>
 #include <iomanip>
-#include <set>
+#include <map>
 #include <cstring>
+#include <set>
+#include <stack>
 #include <bitset>
 #define ll long long
-#define INF 99999999
-#define MAX 7
+#define INF (1e9)
+#define MAX (int) (2e6 + 5)
 #define MOD 1000000007
 #define par pair<int, int>
-#define all(v) (v.begin(), v.end())
+#define all(v) v.begin(), v.end()
+#define sz(x) (int) ((x).size())
+#define esq(x) (x<<1)
+#define dir(x) ((x<<1)|1)
 #define lsb(x) (x & -x)
+#define W(x) cout << #x << ": " << x << endl
+#define Wii(x) cout << x.first << ' ' << x.second << endl
 #define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 using namespace std;
 
-string s;
-int resp, passou[10][10];
+ll n, m, a, b, dp[MAX], fat[MAX];
+vector<par> v;
 
-int e(int a, int b)
+ll fexpo(ll b, ll e)
 {
-	return (a > 0 && b > 0 && a <= 7 && b <= 7 && !passou[a][b]);
+	ll r = 1;
+	while (e)
+	{
+		if (e&1) r = (r * b) % MOD;
+		b = (b * b) % MOD;
+		e>>=1;
+	}
+	return r;
 }
 
-void f(int a, int b, int tam)
+ll f(int a, int b, int c, int d)
 {
-	if (tam == 49) {
-		resp += (a == 7 && b == 1);
-		return;
-	}
-
-	if (a == 7 && b == 1) return;
-
-	passou[a][b] = 1;
-	if (e(a-1, b) && (s[tam-1] == 'U' || s[tam-1] == '?'))
-	{
-		if (!(e(a-1, b+1) && e(a-1, b-1) && !e(a-2, b)))
-			f(a-1, b, tam+1);
-	}
-	if (e(a+1, b) && (s[tam-1] == 'D' || s[tam-1] == '?'))
-	{
-		if (!(e(a+1, b+1) && e(a+1, b-1) && !e(a+2, b)))
-			f(a+1, b, tam+1);
-	}
-	if (e(a, b-1) && (s[tam-1] == 'L' || s[tam-1] == '?'))
-	{
-		if (!(e(a+1, b-1) && e(a-1, b-1) && !e(a, b-2)))
-			f(a, b-1, tam+1);
-	}
-	if (e(a, b+1) && (s[tam-1] == 'R' || s[tam-1] == '?'))
-	{
-		if (!(e(a+1, b+1) && e(a-1, b+1) && !e(a, b+2)))
-			f(a, b+1, tam+1);
-	}
-	passou[a][b] = 0;
+	c = abs(c-a);
+	d = abs(d-b);
+	return (fat[c+d] * fexpo((fat[c]*fat[d]) % MOD, MOD-2)) % MOD;
 }
 
 int main()
 {_
-	cin >> s;
-	f(1, 1, 1);
-	cout << resp << endl;
+	cin >> n >> m;
+	while (m--)
+	{
+		cin >> a >> b;
+		v.push_back({a, b});
+	}
+
+	fat[0] = 1;
+	for (int i = 1; i <= 2*n; i++)
+		fat[i] = (fat[i-1]*i) % MOD;
+
+	v.push_back({n, n});
+	sort(all(v));
+
+	int idx = 0;
+	for (int i = 0; i < sz(v); i++)
+	{
+		auto[a, b] = v[i];
+		dp[i] = f(1, 1, a, b); 
+		for (int j = 0; j < i; j++)
+		{
+			auto[c, d] = v[j];
+			if (d > b) continue;
+			dp[i] = (dp[i] - ((dp[j]*f(a, b, c, d)) % MOD) + MOD) % MOD;
+		}
+	}
+
+	cout << dp[sz(v)-1] << endl;
 }
-
-
-
-
-
 
 
 
