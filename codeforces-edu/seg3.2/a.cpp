e4b05d999d4a151f2d5749807c327b2ef3fa66c6
Luana <luanarj30@gmail.com>
2024-01-19 15:37:45 -0300

    lazy

--- codeforces-edu/seg3.2/a.cpp
+++ codeforces-edu/seg3.2/a.cpp
@@ -1,39 +1,37 @@
 #include <iostream>
-#include <string.h>
+#define MAXN (int) (1e5 + 10)
+#define int long long
 #define esq(x) (x<<1)
 #define dir(x) ((x<<1)|1)
-#define MAXN (int) (1e5 + 10)
 
 using namespace std;
 
-struct preg
+struct tree
 {
-	int flag, seta, soma;
+	int soma, seta, vazio;
 };
-int n, m, op, l, r, v, st[MAXN<<2];
-preg lazy[MAXN<<2];
+
+tree lazy[MAXN<<2], vazio;
+int st[MAXN<<2], n, m, op, l, r, v;
 
 void push(int idx, int i, int j)
 {
-	if (lazy[idx].seta == -1 && lazy[idx].soma == 0) return;
+	if (lazy[idx].vazio) return;
+	st[idx] += (j-i+1)*lazy[idx].soma;
 	if (lazy[idx].seta != -1) st[idx] = (j-i+1)*lazy[idx].seta;
-	else st[idx] += (j-i+1)*lazy[idx].soma;
-	int mid = (j+i)/2;
+
 	if (i != j)
 	{
-		if (lazy[idx].seta != -1)
-		{
-			push(esq(idx), i, mid);
-			push(dir(idx), mid+1, j);
-
-		}
-		lazy[esq(idx)].seta = lazy[dir(idx)].seta = lazy[idx].seta;
+		lazy[esq(idx)].vazio = lazy[dir(idx)].vazio = 0;
 		lazy[esq(idx)].soma += lazy[idx].soma;
 		lazy[dir(idx)].soma += lazy[idx].soma;
-		lazy[esq(idx)].flag |= lazy[idx].flag;
-		lazy[dir(idx)].flag |= lazy[idx].flag;
+		if (lazy[idx].seta != -1) lazy[esq(idx)].seta = lazy[dir(idx)].seta = lazy[idx].seta;
+		
+		if (lazy[esq(idx)].seta != -1 && lazy[idx].seta == -1) lazy[esq(idx)].seta += lazy[idx].soma;
+		if (lazy[dir(idx)].seta != -1 && lazy[idx].seta == -1) lazy[dir(idx)].seta += lazy[idx].soma;
 	}
-	lazy[idx].seta = -1, lazy[idx].soma = lazy[idx].flag = 0;
+
+	lazy[idx] = vazio;
 }
 
 void update(int idx, int i, int j, int l, int r, int v, int flag)
@@ -42,18 +40,22 @@ void update(int idx, int i, int j, int l, int r, int v, int flag)
 	if (i > r || j < l) return;
 	if (i >= l && j <= r)
 	{
-		if (flag) lazy[idx].soma = 0, lazy[idx].seta = v, lazy[idx].flag = 1;
+		lazy[idx].vazio = 0;
+		if (flag) lazy[idx].seta = v;
 		else lazy[idx].soma = v;
 		push(idx, i, j);
+		return;
 	}
 	int mid = (i+j)>>1;
 	update(esq(idx), i, mid, l, r, v, flag);
 	update(dir(idx), mid+1, j, l, r, v, flag);
-	st[idx] = st[esq(idx)] + st[dir(idx)];
+	st[idx] = st[esq(idx)]+st[dir(idx)];
+//	printf("st[%d - %d] == %d\n", i, j, st[idx]);
 }
 
 int query(int idx, int i, int j, int l, int r)
 {
+//	printf("st[%d - %d] == %d\n", i, j, st[idx]);
 	push(idx, i, j);
 	if (i > r || j < l) return 0;
 	if (i >= l && j <= r) return st[idx];
@@ -63,27 +65,36 @@ int query(int idx, int i, int j, int l, int r)
 	return x+y;
 }
 
-int main()
-{	
+void dbug(int l, int r)
+{
+	for (int i = l; i <= r; i++)
+		cout << query(1, 1, n, i, i) << endl;
+	cout << endl;
+}
+
+int32_t main()
+{
 	cin >> n >> m;
+	vazio = {0, -1, 1};
+	for (int i = 0; i < MAXN*4; i++)
+		lazy[i] = vazio;
 
 	while (m--)
 	{
-		cin >> op;
+		cin >> op >> l >> r;
 		if (op == 1)
 		{
-			cin >> l >> r >> v;
-			update(1, 1, n, l, r, v, 1);
+			cin >> v;
+			update(1, 1, n, l+1, r, v, 1);
 		}
-		if (op == 2)
+		else if (op == 2)
 		{
-			cin >> l >> r >> v;
-//			update(1, 1, n, l, r, v, 0);
+			cin >> v;
+			update(1, 1, n, l+1, r, v, 0);
 		}
-		if (op == 3)
+		else
 		{
-			cin >> l >> r;
-//			cout << query(1, 1, n, l, r) << endl;
+			cout << query(1, 1, n, l+1, r) << endl;
 		}
 	}
 }
