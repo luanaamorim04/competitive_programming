1a3dbde27fa3a67e522bbd90ffedc853da58bb0d
Luana <luanarj30@gmail.com>
2024-01-29 11:27:29 -0300

    segtree

--- codeforces-edu/seg3.2/b.cpp
+++ codeforces-edu/seg3.2/b.cpp
@@ -1,73 +1,60 @@
 #include <iostream>
-#define MAXN (int) (1e5 + 10)
+#define MAXN (int) (2e5 + 10)
 #define esq(x) (x<<1)
 #define dir(x) ((x<<1)|1)
 #define int long long
 
 using namespace std;
 
-int st[MAXN<<2], m, n, op, l, r, a, b;
-pair<int, int> lazy[MAXN<<2];
-
-struct L
+struct tree
 {
 	int a1, r, c;
 };
-
-L lazy[MAXN<<2];
+int st[MAXN<<2], m, n, op, l, r, a, b;
+tree lazy[MAXN<<2];
 
 void push(int idx, int i, int j)
 {
-	if(lazy[idx].a1 == 0 && lazy[idx].c == 0) return;
-	int a1 = lazy[idx].a1, an = a1 + lazy[idx].r*(j-i+1);
-//	a1 = an = 0;
-	cout << "a1: " << a1 << " an: " << an << endl;
-	st[idx] += (((a1+an) * (j-i+1))>>1) + lazy[idx].c*(j-i+1);
-	cout << "push: " << st[idx] << endl;
+	if(lazy[idx].r == 0 && lazy[idx].a1 == 0 && lazy[idx].c == 0) return;
+	int a1 = lazy[idx].a1;
+    int	an = a1 + lazy[idx].r*(j-i);
+	st[idx] += (((a1+an) * (j-i+1))/2) + lazy[idx].c*(j-i+1);
+	int mid = (i+j)>>1;
 	if (i != j)
 	{
 		lazy[esq(idx)].c += lazy[idx].c;
 		lazy[dir(idx)].c += lazy[idx].c;
+		lazy[esq(idx)].r += lazy[idx].r;
+		lazy[dir(idx)].r += lazy[idx].r;
 		lazy[esq(idx)].a1 += lazy[idx].a1;
-		lazy[dir(idx)].a1 += lazy[idx].a1 + (lazy[idx].r*(((j+i)>>1)+1))>>1;
+		lazy[dir(idx)].a1 += lazy[idx].a1 + (lazy[idx].r*(mid+1-i));
 	}
 
-	lazy[idx] = {0, 0};
+	lazy[idx] = {0, 0, 0};
 }
 
-1 2 3 4
-1 2 4 8
-
-2 4 7 12
-
-int query(int idx, int i, int j, int l, int r)
+int query(int idx, int i, int j, int pos)
 {
 	push(idx, i, j);
-	if (i > r || j < l) return 0;
-	if (i >= l && j <= r) return st[idx];
+	if (i == j) return st[idx];
 	int mid = (i+j)>>1;
-	int x = query(esq(idx), i, mid, l, r);
-	int y = query(dir(idx), mid+1, j, l, r);
-	return x+y;
+	if (pos <= mid) return query(esq(idx), i, mid, pos);
+	return query(dir(idx), mid+1, j, pos);
 }
 
-void update(int idx, int i, int j, int l, int r, int a, int b)
+void update(int idx, int i, int j, int l, int r, int a1, int ra, int c)
 {
-//	push(idx, i, j);
+	push(idx, i, j);
 	if (i > r || j < l) return;
 	if (i >= l && j <= r)
 	{
-		cout << "update: " << i << ' ' << j << endl;
-		lazy[idx].second = b*(i-l+1);
-		lazy[idx].first = a;
-		cout << lazy[idx].second << "x + " << lazy[idx].first << endl; 
+		lazy[idx].a1 = a1 + (ra*(i-l)), lazy[idx].c = c, lazy[idx].r = ra;
 		push(idx, i, j);
-		cout << "sum: " << st[idx] << endl;
 		return;
 	}
 	int mid = (i+j)>>1;
-	update(esq(idx), i, mid, l, r, a, b);
-	update(dir(idx), mid+1, j, l, r, a, b);
+	update(esq(idx), i, mid, l, r, a1, ra, c);
+	update(dir(idx), mid+1, j, l, r, a1, ra, c);
 	st[idx] = st[esq(idx)] + st[dir(idx)];
 }
 
@@ -80,13 +67,12 @@ int32_t main()
 	   	if (op == 1) 
 		{
 			cin >> l >> r >> a >> b;
-			update(1, 1, n, l, r, a, b);
+			update(1, 1, n, l, r, 0, b, a);
 		}	
 		else
 		{
 			cin >> l;
-			cout << query(1, 1, n, l, l) << endl;
+			cout << query(1, 1, n, l) << endl;
 		}
-		break;
 	}
 }
